9648bf327c8370b7c14202cd662c35d3592af827
Herman Ho <hermanho@users.noreply.github.com>
2023-04-05 01:09:34 +0100

    sm version

--- src/logError.ts
+++ src/logError.ts
@@ -1,5 +1,7 @@
 import { serializeError } from "serialize-error";
 
+// const { serializeError } = await import("serialize-error");
+
 export const logError = () => {
   return serializeError(new Error("boom"));
 };
--- src/provinces.ts
+++ src/provinces.ts
@@ -1,7 +1,7 @@
-import nodeCountries from "node-countries";
+import nodeCountries from "node-countries/build/index";
 
 export const getProvincName = () => {
-  const provinces = nodeCountries.US.provinces;
+  const provinces = nodeCountries.default.US.provinces;
   const foundProvince = provinces.find((p) => p.short === "CA");
   if (foundProvince) {
     return foundProvince.name;
